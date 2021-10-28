7001b32e47434d00daf2e189600f83052196b39f
Yudin Pavel <payudin@ozon.ru>
2021-10-28 17:27:16 +0300

    [init-js] working prot

--- extension.js
+++ extension.js
@@ -1,43 +1,57 @@
 
 const vscode = require('vscode');
-const request = require("request");
+const request = require("request-promise");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	console.log('Congratulations, your extension "oneliner" is now active!');
-
 	let disposable = vscode.commands.registerCommand('oneliner.helloWorld', function () {
 		Search();
 	});
 
 	function Search() {
 		vscode.window.showInputBox({ placeHolder: 'Let\'s find what you 🔍 for' }).then(value => {
-			
 			if (!value) return;
-			vscode.window.showInformationMessage(value);
-			OneLiner();
+			OneLiner(value);
+			return
 		})
 	}
 
-	function OneLiner() {
-		request.get('https://api.onelinerhub.com/search?query=jq', function(err, response, body) {
-			// TODO: дописать
-			console.log(body)
-			vscode.window.showInformationMessage(err);
-		})		
-		vscode.window.showQuickPick(['⬅️ back', '2', '3']).then(value => {
+	async function OneLiner(query) {
+		var results = ['⬅️ back']
+		var matching = [{ 'title': '⬅️ back' }]
+		var response = await request.get('https://api.onelinerhub.com/search?query=' + query)
+
+		var body = JSON.parse(response)
+		for (var i = 0; i < body.length; i++) {
+			results.push(body[i].subject)
+			matching.push({ 'title': body[i].subject, 'url': body[i].url, 'code': body[i].code })
+		}
+		vscode.window.showQuickPick(results, { placeHolder: 'Choose your best' }).then(value => {
 			if (!value) return;
-			vscode.window.showInformationMessage(value);
+			matching.map(key => {
+				if (key.title == value) {
+					OpenWebResult(key)
+				} else if (key.title == '⬅️ back') {
+					Search()
+				} else {
+					vscode.window.showInformationMessage('Something went wrong...')
+				}
+			});
 		})
+	}
+
+	function OpenWebResult(item) {
+		vscode.window.showInformationMessage(item.code, 'Explore').then(() => {
+			vscode.env.openExternal(vscode.Uri.parse(item.url));
+		});
 	}
 
 	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
